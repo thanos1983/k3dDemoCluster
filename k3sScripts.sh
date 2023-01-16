abbf863425fd74ddea5e764de00ac5517321d84d
Athanasios Garyfalos <atga@nnit.com>
2023-01-16 14:47:34 +0100

    Removing commented line.

--- k3sScripts.sh
+++ k3sScripts.sh
@@ -8,10 +8,8 @@ while getopts 'cd' OPTION; do
     case "${OPTION}" in
         c)
             echo "you have supplied the -c 'create' option"
-            myregistryport="5000"
             k3d registry create ${myregistry} --port ${myregistryport}
             k3d cluster create --registry-use k3d-${myregistry}:${myregistryport} -p "8080:80@loadbalancer" --agents 2
-            # k3d cluster create --registry-config my-registries.yaml -p "8080:80@loadbalancer" --agents 2
             ;;
         d)
             echo "you have supplied the -d 'destroy' option"
